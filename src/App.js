1ced86aebb491a7d53fa8fc95f80a13f71b39f0f
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-11-17 00:53:36 +0300

    hghfuygyftc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import { Routes, Route, NavLink } from "react-router-dom";
-import Users from "./routes/UserAndUsers/Users";
-import User from "./routes/UserAndUsers/User";
-import Albums from "./routes/AlbumAndAlbums/Albums";
-import Album from "./routes/AlbumAndAlbums/Album";
+
 import './App.css'
 
 export default function App (){
+
   return (
   <div>
 
-    <nav>
+    {/* <nav>
       <NavLink  className={({isActive})=>isActive?'link-active':''} to="/albums" style={{marginRight: '20px',textDecoration:'none'}}>
         Albums
       </NavLink>
       <NavLink  to="/"  className={({isActive})=>isActive?'link-active':''} style={{marginRight: '20px',textDecoration:'none'}} >
         Users
       </NavLink>
-    </nav>
+    </nav> */}
 
     <Routes>
-        <Route  path = '/'element = {<Users/>}/>
-        <Route  path = '/user/:id'element = {<User/>}/>
-        <Route path = '/albums' element = {<Albums/>}/>
-        <Route path = '/albums/:id' element = {<Album/>}/>
+ 
     </Routes>
 
     

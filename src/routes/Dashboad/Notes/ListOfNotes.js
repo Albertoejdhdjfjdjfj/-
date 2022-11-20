8f05e9eaaf65673cf348e32f349f37e6816dfae1
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-11-21 02:40:11 +0300

    I corrected code

--- src/routes/Dashboad/Notes/ListOfNotes.js
+++ src/routes/Dashboad/Notes/ListOfNotes.js
@@ -1,27 +1,36 @@
-/* eslint-disable */
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+export default function ListOfNotes(userId) {
+  const [notes, setNotes] = useState(false);
 
-export default function ListOfNotes({notes}) {
-  return (
-    <div>
-      <p>Notes</p>
-      <NavLink to="/dashboard/notes/create">Add new note</NavLink>
+  useEffect(() => {
+    fetch(`http://localhost:3001/notes?userId=${userId}`)
+      .then((response) => response.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  if (notes) {
+    return (
       <div>
-        {notes.map((item) => (
-          <div key={item.id}>
-            <NavLink to={`/dashboard/notes/${item.id}`}>
-              {item.title} {item.createdAt}
-            </NavLink>
-            <div>
-              {' '}
-              <NavLink to={`/dashboard/notes/edit/${item.id}`}>Edit</NavLink>{' '}
-              <a onClick={() => deleteNote(item.id)}>Delete</a>
+        <p>Notes</p>
+        <NavLink to="/dashboard/notes/create">Add new note</NavLink>
+        <div>
+          {notes.map((item) => (
+            <div key={item.id}>
+              <NavLink to={`/dashboard/notes/${item.id}`}>
+                {item.title} {item.createdAt}
+              </NavLink>
+              <div>
+                {' '}
+                <NavLink to={`/dashboard/notes/edit/${item.id}`}>Edit</NavLink>{' '}
+                <a onClick={() => deleteNote(item.id)}>Delete</a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function deleteNote(id) {
--- src/routes/Dashboad/Notes/Notes.js
+++ src/routes/Dashboad/Notes/Notes.js
@@ -1,26 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import ListOfNotes from './ListOfNotes';
 import CreateNote from './CreateNote';
 import ViewNote from './ViewNote';
 import EditNote from './EditNote';
 export default function Notes(userId) {
-  const [notes, setNotes] = useState(false);
-
-  useEffect(() => {
-    fetch(`http://localhost:3001/notes?userId=${userId}`)
-      .then((response) => response.json())
-      .then((data) => setNotes(data));
-  }, []);
-
-  if (notes) {
-    return (
-      <Routes>
-        <Route index element={<ListOfNotes notes={notes} />} />
-        <Route path="/create" element={<CreateNote Id={userId} />} />
-        <Route path="/:id" element={<ViewNote />} />
-        <Route path="edit/:id" element={<EditNote />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route index element={<ListOfNotes userId={userId} />} />
+      <Route path="/create" element={<CreateNote Id={userId} />} />
+      <Route path="/:id" element={<ViewNote />} />
+      <Route path="edit/:id" element={<EditNote />} />
+    </Routes>
+  );
 }
